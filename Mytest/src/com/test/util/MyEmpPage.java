6d5e56f75eff272c49d9089c805bf6b2c5d05042
superuser <superuser@WSWINCNHZ1171>
2013-10-09 15:03:06 +0800

    test

---
+++ Mytest/src/com/test/util/MyEmpPage.java
@@ -0,0 +1,30 @@
+package com.test.util;
+
+import com.test.model.Emp;
+
+public class MyEmpPage {
+	private int pageNo;
+	private int pageCount;
+	private String ename;
+	public int getPageNo() {
+		return pageNo;
+	}
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+	public int getPageCount() {
+		return pageCount;
+	}
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+
+
+}
